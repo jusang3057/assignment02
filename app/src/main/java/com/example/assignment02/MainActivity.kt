e9e82ea04dd6e92c1f861ce03d1e4bedd79bfbd9
kimjusang <jusang3057@gmail.com>
2024-11-12 20:26:10 +0900

    실습4
    실습4 2024 11 12

--- app/src/main/java/com/example/assignment02/MainActivity.kt
+++ app/src/main/java/com/example/assignment02/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.assignment02
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
         val toast_btn: TextView = findViewById(R.id.toast_btn)
         val random_btn: TextView = findViewById(R.id.random_btn)
         val intent = Intent(this, RandomActivity::class.java)
+
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK)
+                Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(this, "오류", Toast.LENGTH_SHORT).show()
+        }
 
         count_btn.setOnClickListener {
             number_text.text = (number_text.text.toString().toInt()+1).toString()
--- app/src/main/java/com/example/assignment02/RandomActivity.kt
+++ app/src/main/java/com/example/assignment02/RandomActivity.kt
@@ -10,12 +10,11 @@ class RandomActivity : AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_random)
         val intent = Intent()
-        number = intent.getIntExtra("number", )
+        val number = intent.getIntExtra("number", 0)
 
         // 어떤 작업 후 RandomActivity 종료
-        val random_number = Random.nextInt(0, 100)  // 0부터 99까지의 랜덤 값
+        val random_number = Random.nextInt(0, number)  // 0부터 99까지의 랜덤 값
         intent.putExtra("random_number", random_number)
         setResult(RESULT_OK, intent)
-        finish()
     }
 }
